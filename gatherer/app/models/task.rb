33be9b12466fe9649f905c68877e55ddaf94af2b
miyohide <miyohide@gmail.com>
2015-09-13 22:03:03 +0900

    P32-33. implement total_size method and remaining_size method.

--- gatherer/app/models/task.rb
+++ gatherer/app/models/task.rb
@@ -1,6 +1,12 @@
 class Task
-  def initialize
-    @completed = false
+  attr_accessor :size, :completed
+
+  # このinitializeの処理はActiveRecordで実装していたら不要なもの。
+  # だが、P32に書かれているように今ここでActiveRecordの導入は適して
+  # いないのでまだ実装しない。
+  def initialize(options = {})
+    @completed = options[:completed]
+    @size = options[:size]
   end
 
   def mark_completed
