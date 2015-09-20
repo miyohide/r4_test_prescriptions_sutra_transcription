d16a22d2474f8dfe85345ca5b9553ab9583e2640
miyohide <miyohide@gmail.com>
2015-09-20 22:03:19 +0900

    P37. add project with data test
    複数のタスクを用意して、volocityに関する計算を追加した。

--- gatherer/spec/models/project_spec.rb
+++ gatherer/spec/models/project_spec.rb
@@ -41,4 +41,31 @@ RSpec.describe Project do
       expect(project.remaining_size).to eq(5)
     end
   end
+
+  describe "project with data test" do
+    let(:project) { Project.new }
+    let(:newly_done) { Task.new(size: 3, completed_at: 1.day.ago) }
+    let(:old_done) { Task.new(size: 2, completed_at: 6.months.ago) }
+    let(:small_not_done) { Task.new(size: 1) }
+    let(:large_not_done) { Task.new(size: 4) }
+
+    before(:example) do
+      project.tasks = [newly_done, old_done, small_not_done, large_not_done]
+    end
+
+    it "knows its velocity" do
+      expect(project.comleted_velocity).to eq(3)
+    end
+
+    it "knows its rate" do
+      expect(project.current_rate).to eq(1.0 / 7)
+    end
+
+    it "knows if it is on schedule" do
+      project.due_date = 1.week.from_now
+      expect(project).not_to be_on_schedule
+      project.due_date = 6.months.from_now
+      expect(project).to be_on_schedule
+    end
+  end
 end
