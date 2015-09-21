1c864f7d729acbc2c92f69e830fa99053ece90b0
miyohide <miyohide@gmail.com>
2015-09-21 20:27:55 +0900

    P38. incomplete some methods.

--- gatherer/app/models/project.rb
+++ gatherer/app/models/project.rb
@@ -1,5 +1,5 @@
 class Project
-  attr_accessor :tasks
+  attr_accessor :tasks, :due_date
 
   def initialize
     @tasks = []
@@ -15,5 +15,25 @@ class Project
 
   def remaining_size
     tasks.reject(&:complete?).sum(&:size)
+  end
+
+  # 3週間前に完了したタスクのvelocityの合計を算出する
+  def completed_velocity
+    tasks.sum(&:points_toward_velocity)
+  end
+
+  # 3週間前に完了したタスクのvelocityの1日あたりの値を算出する
+  def current_rate
+    completed_velocity * 1.0 / 21
+  end
+
+  # 残りのタスクのサイズとcurrent_rateから残りの日付を算出する
+  def projected_days_remaining
+    remaining_size / current_rate
+  end
+
+  # オンスケであればtrue、遅延しているならfalseを返す
+  def on_schedule?
+    (Date.today + projected_days_remaining) <= due_date
   end
 end
