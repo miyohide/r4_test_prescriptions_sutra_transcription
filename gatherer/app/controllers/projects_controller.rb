43253425cd82db0ea59859f5b7e20fad29241bfa
miyohide <miyohide@gmail.com>
2015-10-05 21:39:10 +0900

    add create action.

--- gatherer/app/controllers/projects_controller.rb
+++ gatherer/app/controllers/projects_controller.rb
@@ -2,4 +2,13 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+
+  def create
+    @action = CreatesProject.new(
+      name: params[:project][:name],
+      task_string: params[:project][:tasks]
+    )
+    @action.create
+    redirect_to projects_path
+  end
 end
