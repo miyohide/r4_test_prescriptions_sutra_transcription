4bb0c70f09ec95da8c2cce7218224dc973c854cb
miyohide <miyohide@gmail.com>
2015-10-06 23:10:14 +0900

    P58. implement failure case

--- gatherer/app/controllers/projects_controller.rb
+++ gatherer/app/controllers/projects_controller.rb
@@ -8,7 +8,12 @@ class ProjectsController < ApplicationController
       name: params[:project][:name],
       task_string: params[:project][:tasks]
     )
-    @action.create
-    redirect_to projects_path
+    success = @action.create
+    if success
+      redirect_to projects_path
+    else
+      @project = @action.project
+      render :new
+    end
   end
 end
