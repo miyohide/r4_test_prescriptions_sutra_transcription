63b77336dcabdc805a966c8591ac19bfdaa23dac
miyohide <miyohide@gmail.com>
2015-10-22 00:17:42 +0900

    P102. add big_project factory. fix syntax error.

--- gatherer/spec/factories/project.rb
+++ gatherer/spec/factories/project.rb
@@ -3,4 +3,9 @@ FactoryGirl.define do
     name "Project Runway"
     due_date Date.parse("2014-01-12")
   end
+
+  factory :big_project, class: Project do
+    # P102の記載では「name: "Big Project"」だけどそれだとSyntax Errorとなる
+    name "Big Project"
+  end
 end
