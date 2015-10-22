d283dfa68a2a3251a7cc15e34808e8748f0d636d
miyohide <miyohide@gmail.com>
2015-10-22 22:53:02 +0900

    P103. refer to a previously assigned value later in the factory.

--- gatherer/spec/factories/project.rb
+++ gatherer/spec/factories/project.rb
@@ -4,6 +4,11 @@ FactoryGirl.define do
     # ここでは波括弧が必要。ない場合、`-': expected numeric (TypeError)
     # とでてエラーとなる（rails console実行時）
     due_date { Date.today - rand(50) }
+    # これも波括弧が必要。ない場合、undefined method `downcase' for
+    # #<FactoryGirl::Declaration::Implicit:0x007f84f3a1cc58> (NoMethodError)
+    # と出た。（rails console実行時 ）
+    # なお、slugというカラムはないため、buildした時点で落ちる
+    slug "#{name.downcase.gsub(" ", "_")}"
   end
 
   factory :big_project, class: Project do
