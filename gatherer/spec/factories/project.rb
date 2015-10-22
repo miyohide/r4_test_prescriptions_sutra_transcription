41a84ca84a86a20db5420ead489de3abd54c6ad5
miyohide <miyohide@gmail.com>
2015-10-22 22:39:53 +0900

    P102. use block.

--- gatherer/spec/factories/project.rb
+++ gatherer/spec/factories/project.rb
@@ -1,7 +1,9 @@
 FactoryGirl.define do
   factory :project do
     name "Project Runway"
-    due_date Date.parse("2014-01-12")
+    # ここでは波括弧が必要。ない場合、`-': expected numeric (TypeError)
+    # とでてエラーとなる（rails console実行時）
+    due_date { Date.today - rand(50) }
   end
 
   factory :big_project, class: Project do
