8350799775f3b06dd85a0afef2df57b6d5bfa9b1
miyohide <miyohide@gmail.com>
2015-10-24 22:13:34 +0900

    P107. use sequence

--- gatherer/spec/factories/task.rb
+++ gatherer/spec/factories/task.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :task do
-    title "To Something"
+    # title "To Something"
+    # sequenceは連番を生成する
+    sequence(:title) { |n| "Task #{n}"}
     size 3
     # associationは生成すると同時に関連するfactoryを生成する
     # このケースだと、まずprojectが作られて、その後にtaskが作られる
