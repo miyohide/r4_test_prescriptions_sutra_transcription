1135467d902e83ea3b7ef27eb1bdea1430d37225
miyohide <miyohide@gmail.com>
2015-10-24 22:27:32 +0900

    P108-109. inherited factories

--- gatherer/spec/factories/task.rb
+++ gatherer/spec/factories/task.rb
@@ -1,11 +1,14 @@
 FactoryGirl.define do
   factory :task do
-    # title "To Something"
     # sequenceは連番を生成する
     sequence(:title) { |n| "Task #{n}"}
-    size 3
-    # associationは生成すると同時に関連するfactoryを生成する
-    # このケースだと、まずprojectが作られて、その後にtaskが作られる
-    project  # association
+  end
+
+  factory :big_task, parent: :task do
+    size 5
+  end
+
+  factory :small_task, parent: :task do
+    size 1
   end
 end
