90808a708312f0b537a4eba938cbfd5e170d683a
miyohide <miyohide@gmail.com>
2015-10-25 22:36:38 +0900

    P109. nesting factories.

--- gatherer/spec/factories/task.rb
+++ gatherer/spec/factories/task.rb
@@ -2,13 +2,13 @@ FactoryGirl.define do
   factory :task do
     # sequenceは連番を生成する
     sequence(:title) { |n| "Task #{n}"}
-  end
 
-  factory :big_task, parent: :task do
-    size 5
-  end
+    factory :big_task, parent: :task do
+      size 5
+    end
 
-  factory :small_task, parent: :task do
-    size 1
+    factory :small_task, parent: :task do
+      size 1
+    end
   end
 end
