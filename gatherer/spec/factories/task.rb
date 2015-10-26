9005e7d7cf3f51b5021c5f308c53537916e41f29
miyohide <miyohide@gmail.com>
2015-10-26 23:10:19 +0900

    P109-110. use trait.

--- gatherer/spec/factories/task.rb
+++ gatherer/spec/factories/task.rb
@@ -3,12 +3,34 @@ FactoryGirl.define do
     # sequenceは連番を生成する
     sequence(:title) { |n| "Task #{n}"}
 
-    factory :big_task, parent: :task do
+    # traitは1つの属性だけの意味付けって感じ。
+    # これだけでFactoryGirl.build(:small)ってやっても
+    # Factory not registeredが出て落ちてしまう
+    trait :small do
+      size 1
+    end
+
+    trait :large do
       size 5
     end
 
-    factory :small_task, parent: :task do
-      size 1
+    trait :soon do
+      due_date { 1.day.from_now }
+    end
+
+    trait :later do
+      due_date { 1.month.from_now }
+    end
+
+    # traitの使い方。traitを並べてFactoryとして定義するって形
+    factory :trivial do
+      small
+      later
+    end
+
+    factory :panic do
+      large
+      soon
     end
   end
 end
