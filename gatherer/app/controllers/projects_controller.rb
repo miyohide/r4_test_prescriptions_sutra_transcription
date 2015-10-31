13da378a10c4b0bf93c3dedee6f5ade2c8e01c30
miyohide <miyohide@gmail.com>
2015-10-31 23:28:59 +0900

    P127. create update action.

--- gatherer/app/controllers/projects_controller.rb
+++ gatherer/app/controllers/projects_controller.rb
@@ -20,4 +20,13 @@ class ProjectsController < ApplicationController
       render :new
     end
   end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(params[:project])
+      redirect_to @project, notice: "'project was successfully updated.'"
+    else
+      render action: "edit"
+    end
+  end
 end
