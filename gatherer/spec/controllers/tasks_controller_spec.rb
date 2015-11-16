3d6ba3f64fadeafb44c7e4cb795f8d55185c60d6
miyohide <miyohide@gmail.com>
2015-11-16 23:31:15 +0900

    P156. add mailer spec.

--- gatherer/spec/controllers/tasks_controller_spec.rb
+++ gatherer/spec/controllers/tasks_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
+  before(:example) do
+    ActionMailer::Base.deliveries.clear
+  end
+
+  describe "PATCH update" do
+    let(:task) { Task.create!(title: "Write section on testing mailers", size: 2) }
+    it "does not send an email if a task is not completed" do
+      patch :update, id: task.id, task: {size: 3}
+      expect(ActionMailer::Base.deliveries.size).to eq(0)
+    end
+  end
 
 end
