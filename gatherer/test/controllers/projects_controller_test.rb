df3e75326df4ee156c7b6242773fbf7f0d8bf92b
miyohide <miyohide@gmail.com>
2015-12-02 23:30:50 +0900

    P169. add projects controller test.

--- gatherer/test/controllers/projects_controller_test.rb
+++ gatherer/test/controllers/projects_controller_test.rb
@@ -6,4 +6,16 @@ class ProjectsControllerTest < ActiveSupport::TestCase
     assert_redirected_to projects_path
     assert_equal "Runway", assigns[:action].project.name
   end
+
+  test "the index method displays all projects correctly" do
+    on_schedule = Project.create!(due_date: 1.year.from_now,
+        name: "On Schedule",
+        tasks: [Task.create!(completed_at: 1.day.ago, size: 1)])
+    behind_schedule = Project.create!(due_date: 1.day.from_now,
+        name: "Behind Schedule",
+        tasks: [Task.create!(size: 1)])
+    get :index
+    assert_select("#project_#{on_schedule.id} .on_schedule")
+    assert_select("#project_#{behind_schedule.id} .behind_schedule")
+  end
 end
