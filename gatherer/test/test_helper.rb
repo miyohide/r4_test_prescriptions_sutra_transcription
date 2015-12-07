fc71eef4ee45a11d33a6e1176f40a935101ffa80
miyohide <miyohide@gmail.com>
2015-12-08 00:11:48 +0900

    P172. add assert_select_string.

--- gatherer/test/test_helper.rb
+++ gatherer/test/test_helper.rb
@@ -3,3 +3,8 @@ require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails/capybara"
 require "mocha/mini_test"
+
+def assert_select_string(string, *selectors, &block)
+  doc_root = HTML::Document.new(string).root
+  assert_select(doc_root, *selectors, &block)
+end
