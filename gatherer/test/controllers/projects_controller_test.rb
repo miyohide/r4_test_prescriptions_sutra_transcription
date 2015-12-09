b9b0ad19baaf137b013b9b64d521304ace8395b0
miyohide <miyohide@gmail.com>
2015-12-10 00:44:27 +0900

    move the routing test in a separate file.

--- gatherer/test/controllers/projects_controller_test.rb
+++ gatherer/test/controllers/projects_controller_test.rb
@@ -18,19 +18,4 @@ class ProjectsControllerTest < ActiveSupport::TestCase
     assert_select("#project_#{on_schedule.id} .on_schedule")
     assert_select("#project_#{behind_schedule.id} .behind_schedule")
   end
-
-  test "routing" do
-    assert_routing "/projects", controller: "projects", action: "index"
-    assert_routing({path: "/projects", method: "post"},
-        controller: "projects", action: "create")
-    assert_routing "/projects/new", controller: "projects", action: "new"
-    assert_routing "/projects/1", controller: "projects",
-        action: "show", id: "1"
-    assert_routing "/projects/1/edit", controller: "projects",
-        action: "edit", id: "1"
-    assert_routing({path: "/projects/1", method: "patch"},
-        controller: "projects", action: "update", id: "1")
-    assert_routing({path: "/projects/1", method: "delete"},
-        controller: "projects", action: "destroy", id: "1")
-  end
 end
