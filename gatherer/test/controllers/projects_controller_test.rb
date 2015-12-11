0f8b00eaa7ba7876e581e078ed335516d5b285be
miyohide <miyohide@gmail.com>
2015-12-12 00:41:41 +0900

    NoMethodError: undefined method get/post

--- gatherer/test/controllers/projects_controller_test.rb
+++ gatherer/test/controllers/projects_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class ProjectsControllerTest < ActiveSupport::TestCase
+class ProjectsControllerTest < ActionController::TestCase
   test "the project method creates a project" do
     post :create, project: {name: "Runway", tasks: "start something:2"}
     assert_redirected_to projects_path
--- gatherer/test/test_helper.rb
+++ gatherer/test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
+require "rack/test"
 require "minitest/rails/capybara"
 require "mocha/mini_test"
 
