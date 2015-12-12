80e36c37ea37cbeb4bc436eb1d708a7c84fc2772
miyohide <miyohide@gmail.com>
2015-12-13 00:35:39 +0900

    delete rack/test

--- gatherer/test/test_helper.rb
+++ gatherer/test/test_helper.rb
@@ -1,11 +1,12 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
-require "rack/test"
 require "minitest/rails/capybara"
 require "mocha/mini_test"
 
-def assert_select_string(string, *selectors, &block)
-  doc_root = HTML::Document.new(string).root
-  assert_select(doc_root, *selectors, &block)
+class ActiveSupport::TestCase
+  def assert_select_string(string, *selectors, &block)
+    doc_root = HTML::Document.new(string).root
+    assert_select(doc_root, *selectors, &block)
+  end
 end
