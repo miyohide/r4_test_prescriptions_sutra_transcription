fa264cf950d9e843ebd59b8aa6b2f9e34ac70b56
miyohide <miyohide@gmail.com>
2016-02-19 23:17:57 +0900

    P188. Add create action.

--- gatherer/app/controllers/tasks_controller.rb
+++ gatherer/app/controllers/tasks_controller.rb
@@ -14,4 +14,11 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def create
+    @task = Task.new(
+      params[:task].permit(:project_id, :title, :size)
+    )
+    redirect_to @task.project
+  end
 end
