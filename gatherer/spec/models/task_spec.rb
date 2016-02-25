5133c21869e43d5c16b42645b03da9e8b85693af
miyohide <miyohide@gmail.com>
2016-02-25 23:10:22 +0900

    add task order spec

--- gatherer/spec/models/task_spec.rb
+++ gatherer/spec/models/task_spec.rb
@@ -42,4 +42,20 @@ RSpec.describe Task do
       expect(task.points_toward_velocity).to eq(3)
     end
   end
+
+  describe "order" do
+    let!(:project) { Project.create!(name: "Project") }
+    let!(:first) { project.tasks.create!(project_order: 1) }
+    let!(:second) { project.tasks.create!(project_order: 2) }
+    let!(:third) { project.tasks.create!(project_order: 3) }
+
+    it "finds that a task is first or last" do
+      expect(first).to be_first_in_project
+      expect(first).not_to be_last_in_project
+      expect(second).not_to be_first_in_project
+      expect(second).not_to be_last_in_project
+      expect(third).not_to be_first_in_project
+      expect(third).to be_last_in_project
+    end
+  end
 end
