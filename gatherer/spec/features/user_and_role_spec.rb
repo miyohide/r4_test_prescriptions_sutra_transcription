e7795603376311f698a88d27d1a67e6768c0ada0
miyohide <miyohide@gmail.com>
2016-03-20 23:43:06 +0900

    P208. add integration test for the project index page.

---
+++ gatherer/spec/features/user_and_role_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+describe "with users and roles" do
+  def log_in_as(user)
+    visit new_user_session_path
+    fill_in("user_email", with: user.email)
+    fill_in("user_password", with: user.password)
+    click_button("Log in")
+  end
+
+  let(:user) { User.crate(email: "test@example.com", password: "password") }
+
+  it "allows a logged-in user to view the project index page" do
+    log_in_as(user)
+    visit(projects_path)
+    expect(current_path).to eq(projects_path)
+  end
+end
