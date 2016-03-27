343456c0c7f331ffc1de066b7fe56149ee096728
miyohide <miyohide@gmail.com>
2016-03-27 20:21:33 +0900

    P213. rails g model role.

--- gatherer/db/schema.rb
+++ gatherer/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160316125742) do
+ActiveRecord::Schema.define(version: 20160327111515) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,17 @@ ActiveRecord::Schema.define(version: 20160316125742) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "roles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "project_id"
+    t.string   "role_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "roles", ["project_id"], name: "index_roles_on_project_id"
+  add_index "roles", ["user_id"], name: "index_roles_on_user_id"
 
   create_table "tasks", force: :cascade do |t|
     t.integer  "project_id"
