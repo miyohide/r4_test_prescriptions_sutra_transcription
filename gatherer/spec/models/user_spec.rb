eb9cb8f5505c76249f96cb9e4e699a26e6b4d972
miyohide <miyohide@gmail.com>
2016-04-01 23:09:46 +0900

    P216. add unit tests.

--- gatherer/spec/models/user_spec.rb
+++ gatherer/spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "cannot view a project it is not a part of" do
+    user = User.new
+    project = Project.new
+    expect(user.can_view?(project)).to be_falsy
+  end
+
+  it "can view a project it is a part of" do
+    user = User.create!(email: "user@example.com", password: "password")
+    project = Project.create!(name: "Project Gutenberg")
+    user.roles.create(project: project)
+    expect(user.can_view?(project)).to be_truthy
+  end
+
+  describe "public roles" do
+    let(:user) { User.new }
+    let(:project) { Project.new }
+
+    it "allows an admin to view a project" do
+      user.admin = true
+      expect(user.can_view?(project)).to be_truthy
+    end
+
+    it "allows a public project to be seen by anyone" do
+      project.public = true
+      expect(user.can_view?(project)).to be_truthy
+    end
+  end
 end
