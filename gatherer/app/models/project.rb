202eff4257f71b05485269409d1e083c6a6f6f96
miyohide <miyohide@gmail.com>
2016-04-22 23:43:34 +0900

    P219. add all_public method.

--- gatherer/app/models/project.rb
+++ gatherer/app/models/project.rb
@@ -48,4 +48,8 @@ class Project < ActiveRecord::Base
     return 1 if tasks.empty?
     (tasks.last.project_order || tasks.size) + 1
   end
+
+  def self.all_public
+    where(public: true)
+  end
 end
