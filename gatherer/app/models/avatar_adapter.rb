32095fbc4b82b7e79b855273b9660d160fe74c54
miyohide <miyohide@gmail.com>
2016-07-22 23:48:16 +0900

    P240. add adapter test and adapter.

--- gatherer/app/models/avatar_adapter.rb
+++ gatherer/app/models/avatar_adapter.rb
@@ -1,7 +1,18 @@
 class AvatarAdapter
+  attr_accessor :user, :client
+
   def initialize(user)
+    @user = user
+  end
+
+  def client
+    @client ||= Twitter::REST::Client.new(
+      consumer_key: Rails.application.secrets.twitter_api,
+      consumer_secret: Rails.application.secrets.twitter_secret
+    )
   end
 
   def image_url
+    client.user(user.twitter_handle).profile_image_uri(:bigger).to_s
   end
 end
